04b148dac8401014ff84d6db4da0572a6ee09ca6
Bamontan <baptiste.demontangon@gmail.com>
2021-06-10 22:13:45 +0200

    redo css for bottom and right nodes

--- src/components/grid.tsx
+++ src/components/grid.tsx
@@ -1,6 +1,6 @@
 // eslint-disable-next-line no-use-before-define
 import React from "react";
-import { coordToString } from "../common";
+import { COL_NUMBER, coordToString, ROW_NUMBER } from "../common";
 import NodeClass from "../NodeClass";
 import Node from "./node";
 
@@ -17,12 +17,14 @@ export default function Grid({
 }:GridProps) {
   return (
     <div id="grid" className="noselect" onContextMenu={(e) => e.preventDefault()}>
-      {nodeGrid.map((row) => row.map((node) => (
+      {nodeGrid.map((row, rowNbr) => row.map((node, colNbr) => (
         <Node
           key={coordToString(node.pos)}
           node={node}
           handleMouseDown={handleMouseDown}
           handleMouseEnter={handleMouseEnter}
+          bottom={rowNbr === ROW_NUMBER - 1}
+          right={colNbr === COL_NUMBER - 1}
         />
       )))}
     </div>
--- src/components/node.tsx
+++ src/components/node.tsx
@@ -7,14 +7,24 @@ interface NodeProps {
   node:NodeClass;
   handleMouseDown:(node:NodeClass, button:number) => void;
   handleMouseEnter:(node:NodeClass) => void;
+  bottom:boolean;
+  right:boolean;
 }
 
-export default function Node({ node, handleMouseDown, handleMouseEnter }:NodeProps) {
+export default function Node({
+  node,
+  handleMouseDown,
+  handleMouseEnter,
+  bottom,
+  right,
+}:NodeProps) {
   const [classNames, setClassNames] = useState<string>("node");
   const changeStateHandler = (newState:NodeState, weight?:number) => {
     const newClassNames = ["node"];
     if (newState !== "empty") newClassNames.push(`node-${node.state}`);
     if (weight) newClassNames.push("node-weight");
+    if (bottom) newClassNames.push(node.state !== "empty" ? `node-${node.state}-bottom` : "node-bottom");
+    if (right) newClassNames.push(node.state !== "empty" ? `node-${node.state}-right` : "node-right");
     setClassNames(newClassNames.join(" "));
   };
   useEffect(() => {
